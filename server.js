7ded12fe8515212b2ebf3d56fef258568499cc3a
g-alita <alita.gope@gmail.com>
2025-10-07 15:29:00 +0530

    server

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-
 
 const express = require('express');
 const mysql = require('mysql2');
